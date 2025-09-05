e781201b4ab206937c06601638dff775bf42b9a9
himanshu jha <jhahiamsnhu930@gmail.com>
2025-09-06 00:26:27 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // ✅ CORS setup
 app.use(cors({
-  origin: "http://localhost:5173" || "https://zipacres-v-1.onrender.com", // frontend URL
+  origin: "http://localhost:5173" || "https://zipacres.vercel.app/", // frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 }));
